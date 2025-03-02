b7c69bf0a6de984551cb0425252f0c8cbeb21436
nat3 <billing.nate@gmx.de>
2025-03-02 21:46:55 +0100

    kkk

--- src/lib.zig
+++ src/lib.zig
@@ -1,4 +1,5 @@
 pub const codegen = @import("delegator.zig");
+pub const prim = @import("primitives/primitives.zig");
 pub const spsc = @import("primitives/weakrb-spsc.zig");
 pub const threads = @import("threads/lib.zig");
 pub const stats = @import("util/statistics.zig");
---
+++ src/primitives/primitives.zig
@@ -0,0 +1,54 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+/// for safe use of from two threads (T is either available at the local thread or at the second thread)
+pub fn OneAccessToT(T: type) type {
+    return struct {
+        const This = @This();
+        thread_t: ?*T = null,
+        local_t: ?*T = null,
+        /// dont attempt to use T passed into this directly
+        pub fn init(alloc: Allocator, inst: T) !*This {
+            const self = try alloc.create(This);
+            const ubf = try alloc.create(T);
+            ubf.* = inst;
+            var this = This{};
+            @atomicStore(?*T, &this.local_t, ubf, .release);
+            self.* = this;
+            return self;
+        }
+        pub fn deinit(self: *This, alloc: Allocator) void {
+            if (self.local_get()) |d| {
+                d.deinit(alloc);
+                alloc.destroy(d);
+            } else {
+                self.thread_get().?.deinit(alloc);
+                alloc.destroy(self.thread_get().?);
+            }
+            alloc.destroy(self);
+        }
+        /// this should be called on the local thread
+        pub fn local_get(self: *This) ?*T {
+            const mb = @atomicLoad(?*T, &self.local_t, .acquire);
+            return mb;
+        }
+        /// this should be called on the local thread
+        /// if you finished using T from local_get, submit it so the second thread can load it
+        pub fn local_submit(self: *This) void {
+            @atomicStore(?*T, &self.thread_t, self.local_t, .release);
+            @atomicStore(?*T, &self.local_t, null, .release);
+        }
+
+        /// this should be called on the second thread
+        pub fn thread_get(self: *This) ?*T {
+            const mb = @atomicLoad(?*T, &self.thread_t, .acquire);
+            return mb;
+        }
+        /// this should be called on the second thread
+        /// if you finished using T from thread_get, submit it so the local thread can load it
+        pub fn thread_submit(self: *This) void {
+            @atomicStore(?*T, &self.local_t, self.thread_t, .release);
+            @atomicStore(?*T, &self.thread_t, null, .release);
+        }
+    };
+}
