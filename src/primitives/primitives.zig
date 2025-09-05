6736ab5edaaf7cbeb39cc1ac713ad07707939343
nat3 <nat3Github>
2025-09-05 16:48:50 +0200

    ok

--- src/primitives/primitives.zig
+++ src/primitives/primitives.zig
@@ -3,56 +3,86 @@ const builtin = @import("builtin");
 const Allocator = std.mem.Allocator;
 const root = @import("../root.zig");
 
-/// for safe use of from two threads (T is either available at the local thread or at the second thread)
-/// NOTE: when using a T which allocates you have to manually free that, this does not call deinit on T
-pub fn OneAccessToT(T: type) type {
+/// writer loses data
+/// reader always reads data
+/// for simple updates where T is to big for Atomics
+pub fn LossyWriter(T: type) type {
     return struct {
         const This = @This();
-        thread_t: ?*T = null,
-        local_t: ?*T = null,
-        /// dont attempt to use T passed into this directly
-        pub fn init(alloc: Allocator, inst: T) !*This {
-            const self = try alloc.create(This);
-            const ubf = try alloc.create(T);
-            ubf.* = inst;
-            var this = This{};
-            @atomicStore(?*T, &this.local_t, ubf, .release);
-            self.* = this;
-            return self;
-        }
-        pub fn deinit(self: *This, alloc: Allocator) void {
-            if (self.local_get()) |d| {
-                alloc.destroy(d);
-            } else {
-                alloc.destroy(self.thread_get().?);
-            }
-            alloc.destroy(self);
-        }
-        /// this should be called on the local thread
-        pub fn local_get(self: *This) ?*T {
-            const mb = @atomicLoad(?*T, &self.local_t, .acquire);
-            return mb;
-        }
-        /// this should be called on the local thread
-        /// if you finished using T from local_get, submit it so the second thread can load it
-        pub fn local_submit(self: *This) void {
-            @atomicStore(?*T, &self.thread_t, self.local_t, .release);
-            @atomicStore(?*T, &self.local_t, null, .release);
-        }
+        raw: T = undefined,
+        mtx: root.prim.Spinlock = .{},
 
-        /// this should be called on the second thread
-        pub fn thread_get(self: *This) ?*T {
-            const mb = @atomicLoad(?*T, &self.thread_t, .acquire);
-            return mb;
+        pub fn init(val: T) @This() {
+            return .{
+                .raw = val,
+            };
         }
-        /// this should be called on the second thread
-        /// if you finished using T from thread_get, submit it so the local thread can load it
-        pub fn thread_submit(self: *This) void {
-            @atomicStore(?*T, &self.local_t, self.thread_t, .release);
-            @atomicStore(?*T, &self.thread_t, null, .release);
+        /// multiple readers are allowed
+        pub fn load(self: *This) T {
+            self.mtx.lock();
+            defer self.mtx.unlock();
+            return self.raw;
+        }
+        /// only single writer
+        pub fn try_write(self: *This, val: T) void {
+            if (self.mtx.try_lock()) {
+                defer self.mtx.unlock();
+                self.raw = val;
+            }
         }
     };
 }
+
+/// for safe use of from two threads (T is either available at the local thread or at the second thread)
+/// NOTE: when using a T which allocates you have to manually free that, this does not call deinit on T
+// pub fn OneAccessToT(T: type) type {
+//     return struct {
+//         const This = @This();
+//         thread_t: ?*T = null,
+//         local_t: ?*T = null,
+//         /// dont attempt to use T passed into this directly
+//         pub fn init(alloc: Allocator, inst: T) !*This {
+//             const self = try alloc.create(This);
+//             const ubf = try alloc.create(T);
+//             ubf.* = inst;
+//             var this = This{};
+//             @atomicStore(?*T, &this.local_t, ubf, .release);
+//             self.* = this;
+//             return self;
+//         }
+//         pub fn deinit(self: *This, alloc: Allocator) void {
+//             if (self.local_get()) |d| {
+//                 alloc.destroy(d);
+//             } else {
+//                 alloc.destroy(self.thread_get().?);
+//             }
+//             alloc.destroy(self);
+//         }
+//         /// this should be called on the local thread
+//         pub fn local_get(self: *This) ?*T {
+//             const mb = @atomicLoad(?*T, &self.local_t, .acquire);
+//             return mb;
+//         }
+//         /// this should be called on the local thread
+//         /// if you finished using T from local_get, submit it so the second thread can load it
+//         pub fn local_submit(self: *This) void {
+//             @atomicStore(?*T, &self.thread_t, self.local_t, .release);
+//             @atomicStore(?*T, &self.local_t, null, .release);
+//         }
+
+//         /// this should be called on the second thread
+//         pub fn thread_get(self: *This) ?*T {
+//             const mb = @atomicLoad(?*T, &self.thread_t, .acquire);
+//             return mb;
+//         }
+//         /// this should be called on the second thread
+//         /// if you finished using T from thread_get, submit it so the local thread can load it
+//         pub fn thread_submit(self: *This) void {
+//             @atomicStore(?*T, &self.local_t, self.thread_t, .release);
+//             @atomicStore(?*T, &self.thread_t, null, .release);
+//         }
+//     };
+// }
 
 /// this lock is safe to use in a audio realtime scenario
 /// use this when you want to mutate variables of something accesed by the realtime thread
